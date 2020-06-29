5f7958475c6f0c7b17405e97d1a93f52c810fbcb
doege2020 <62788161+doege2020@users.noreply.github.com>
2020-06-29 12:06:30 +0300

    Add files via upload

--- egeclock.js
+++ egeclock.js
@@ -1,4 +1,12 @@
-var startDate = new Date("June 17, 2020 10:00:00").getTime();   
+/*
+
+    Нахуй сюда-то смотреть?
+    Иди ботай блять
+
+*/
+
+
+const startDate = new Date("June 17, 2020 10:00:00").getTime();   
 
 
 
@@ -68,6 +76,7 @@ function Inf_clockege() {
     var distance = Inf_countDownDate - now;
 
     if (distance <= 0) {
+        /*
         document.getElementById("inf-countdown-weeks").innerHTML = 0; 
         document.getElementById("inf-countdown-days").innerHTML = 0; 
         document.getElementById("inf-countdown-hours").innerHTML = 0; 
@@ -75,6 +84,12 @@ function Inf_clockege() {
         document.getElementById("inf-countdown-seconds").innerHTML = 0; 
 
         document.getElementById("inf-egeprogress").value = 100;
+        */
+
+        document.getElementById("inf-countdown").style.display = "none";
+        document.getElementById("inf-egeprogress").style.display = "none";
+
+        document.getElementById("inf-notify").style.display = "block";
     }
             
     else{
@@ -256,4 +271,410 @@ function Phys_clockege() {
     }
 
     window.setTimeout('Phys_clockege()', 1);
+}
+
+
+var Soc_countDownDate = new Date("July 16, 2020 10:00:00").getTime();
+
+var Soc_delta_startdown = Phys_countDownDate - startDate;
+
+Soc_clockege();
+
+function Soc_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = Soc_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("soc-countdown-weeks").innerHTML = 0; 
+        document.getElementById("soc-countdown-days").innerHTML = 0; 
+        document.getElementById("soc-countdown-hours").innerHTML = 0; 
+        document.getElementById("soc-countdown-minutes").innerHTML = 0; 
+        document.getElementById("soc-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("soc-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/Soc_delta_startdown;
+
+        document.getElementById("soc-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("soc-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("soc-countdown-days").innerHTML = days; 
+        document.getElementById("soc-countdown-hours").innerHTML = hours; 
+        document.getElementById("soc-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("soc-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('Soc_clockege()', 1);
+}
+
+
+var Chem_countDownDate = new Date("July 16, 2020 10:00:00").getTime();
+
+var Chem_delta_startdown = Chem_countDownDate - startDate;
+
+Chem_clockege();
+
+function Chem_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = Chem_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("chem-countdown-weeks").innerHTML = 0; 
+        document.getElementById("chem-countdown-days").innerHTML = 0; 
+        document.getElementById("chem-countdown-hours").innerHTML = 0; 
+        document.getElementById("chem-countdown-minutes").innerHTML = 0; 
+        document.getElementById("chem-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("chem-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/Chem_delta_startdown;
+
+        document.getElementById("chem-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("chem-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("chem-countdown-days").innerHTML = days; 
+        document.getElementById("chem-countdown-hours").innerHTML = hours; 
+        document.getElementById("chem-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("chem-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('Chem_clockege()', 1);
+}
+
+
+var Bio_countDownDate = new Date("July 20, 2020 10:00:00").getTime();
+
+var Bio_delta_startdown = Bio_countDownDate - startDate;
+
+Bio_clockege();
+
+function Bio_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = Bio_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("bio-countdown-weeks").innerHTML = 0; 
+        document.getElementById("bio-countdown-days").innerHTML = 0; 
+        document.getElementById("bio-countdown-hours").innerHTML = 0; 
+        document.getElementById("bio-countdown-minutes").innerHTML = 0; 
+        document.getElementById("bio-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("bio-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/Bio_delta_startdown;
+
+        document.getElementById("bio-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("bio-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("bio-countdown-days").innerHTML = days; 
+        document.getElementById("bio-countdown-hours").innerHTML = hours; 
+        document.getElementById("bio-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("bio-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('Bio_clockege()', 1);
+}
+
+
+var Lang20_countDownDate = new Date("July 20, 2020 10:00:00").getTime();
+
+var Lang20_delta_startdown = Lang20_countDownDate - startDate;
+
+Lang20_clockege();
+
+function Lang20_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = Lang20_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("lang20-countdown-weeks").innerHTML = 0; 
+        document.getElementById("lang20-countdown-days").innerHTML = 0; 
+        document.getElementById("lang20-countdown-hours").innerHTML = 0; 
+        document.getElementById("lang20-countdown-minutes").innerHTML = 0; 
+        document.getElementById("lang20-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("lang20-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/Lang20_delta_startdown;
+
+        document.getElementById("lang20-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("lang20-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("lang20-countdown-days").innerHTML = days; 
+        document.getElementById("lang20-countdown-hours").innerHTML = hours; 
+        document.getElementById("lang20-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("lang20-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('Lang20_clockege()', 1);
+}
+
+
+var Lang22_countDownDate = new Date("July 22, 2020 10:00:00").getTime();
+
+var Lang22_delta_startdown = Lang22_countDownDate - startDate;
+
+Lang22_clockege();
+
+function Lang22_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = Lang22_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("lang22-countdown-weeks").innerHTML = 0; 
+        document.getElementById("lang22-countdown-days").innerHTML = 0; 
+        document.getElementById("lang22-countdown-hours").innerHTML = 0; 
+        document.getElementById("lang22-countdown-minutes").innerHTML = 0; 
+        document.getElementById("lang22-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("lang22-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/Lang22_delta_startdown;
+
+        document.getElementById("lang22-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("lang22-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("lang22-countdown-days").innerHTML = days; 
+        document.getElementById("lang22-countdown-hours").innerHTML = hours; 
+        document.getElementById("lang22-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("lang22-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('Lang22_clockege()', 1);
+}
+
+
+var Lang23_countDownDate = new Date("July 23, 2020 10:00:00").getTime();
+
+var Lang23_delta_startdown = Lang23_countDownDate - startDate;
+
+Lang23_clockege();
+
+function Lang23_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = Lang23_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("lang23-countdown-weeks").innerHTML = 0; 
+        document.getElementById("lang23-countdown-days").innerHTML = 0; 
+        document.getElementById("lang23-countdown-hours").innerHTML = 0; 
+        document.getElementById("lang23-countdown-minutes").innerHTML = 0; 
+        document.getElementById("lang23-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("lang23-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/Lang23_delta_startdown;
+
+        document.getElementById("lang23-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("lang23-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("lang23-countdown-days").innerHTML = days; 
+        document.getElementById("lang23-countdown-hours").innerHTML = hours; 
+        document.getElementById("lang23-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("lang23-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('Lang23_clockege()', 1);
+}
+
+var His_countDownDate = new Date("July 13, 2020 10:00:00").getTime();
+
+var His_delta_startdown = His_countDownDate - startDate;
+
+His_clockege();
+
+function His_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = His_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("his-countdown-weeks").innerHTML = 0; 
+        document.getElementById("his-countdown-days").innerHTML = 0; 
+        document.getElementById("his-countdown-hours").innerHTML = 0; 
+        document.getElementById("his-countdown-minutes").innerHTML = 0; 
+        document.getElementById("his-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("his-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/His_delta_startdown;
+
+        document.getElementById("his-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("his-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("his-countdown-days").innerHTML = days; 
+        document.getElementById("his-countdown-hours").innerHTML = hours; 
+        document.getElementById("his-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("his-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('His_clockege()', 1);
+}
+
+var Lit_countDownDate = new Date("July 3, 2020 10:00:00").getTime();
+
+var Lit_delta_startdown = Lit_countDownDate - startDate;
+
+Lit_clockege();
+
+function Lit_clockege() {
+
+    var now = new Date().getTime();
+    
+    var distance = Lit_countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("lit-countdown-weeks").innerHTML = 0; 
+        document.getElementById("lit-countdown-days").innerHTML = 0; 
+        document.getElementById("lit-countdown-hours").innerHTML = 0; 
+        document.getElementById("lit-countdown-minutes").innerHTML = 0; 
+        document.getElementById("lit-countdown-seconds").innerHTML = 0; 
+
+        document.getElementById("lit-egeprogress").value = 100;
+    }
+            
+    else{
+
+        var progress = (now - startDate)/Lit_delta_startdown;
+
+        document.getElementById("lit-egeprogress").value = progress*100;
+
+        //console.log(progress.toFixed(3))
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(days / 7);
+        days = days - weeks * 7;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconds = Math.floor((distance % (1000 * 60)));
+
+        var deltaMilliseconds = milliseconds - seconds*1000;
+
+        document.getElementById("lit-countdown-weeks").innerHTML = weeks; 
+        document.getElementById("lit-countdown-days").innerHTML = days; 
+        document.getElementById("lit-countdown-hours").innerHTML = hours; 
+        document.getElementById("lit-countdown-minutes").innerHTML = minutes; 
+        document.getElementById("lit-countdown-seconds").innerHTML = seconds; 
+    }
+
+    window.setTimeout('Lit_clockege()', 1);
 }
